53fccc69b9e19a8eed2718ba48f87fc3d38443ea
Philipp Sanktjohanser <venture@mein.gmx>
2023-02-01 07:05:39 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Philipp Sanktjohanser on 01.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
